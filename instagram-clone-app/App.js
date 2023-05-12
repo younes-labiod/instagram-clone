3f6da24e4671524410007e313beecc8d8d20726e
younes-labiod <youneslabiod@gmail.com>
2023-05-13 00:46:59 +0400

    Insta Clone | Image rendering and code refactoring

--- instagram-clone-app/App.js
+++ instagram-clone-app/App.js
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import InstagramClone from "./instagram-clone/InstagramClone";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your Insta clone app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <InstagramClone />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- instagram-clone-app/instagram-clone/data.js
+++ instagram-clone-app/instagram-clone/data.js
@@ -1,25 +1,25 @@
 const initialData = {
     profile: {
-        avatar: require("../assets/instagram-feed-images/avatar.png")
+        avatar: require("../assets/instagram-clone-images/avatar.jpeg")
     },
     stories: [
         {
             id:1,
-            avatar: require("../assets/instagram-feed-images/avatar1.jpg"),
-            name: '5.min.craft',
+            avatar: require("../assets/instagram-clone-images/avatar1.jpg"),
+            name: 'Younes Labiod',
             isSeen: false,
         },
 
         {
             id:2,
-            avatar: require("../assets/instagram-feed-images/avatar2.jpg"),
+            avatar: require("../assets/instagram-clone-images/avatar2.jpg"),
             name: 'tommy girl',
             isSeen: false,
         },
 
         {
             id:3,
-            avatar: require("../assets/instagram-feed-images/avatar3.jpg"),
+            avatar: require("../assets/instagram-clone-images/avatar3.jpg"),
             name: 'ninja mertyle',
             isSeen: true
         },
@@ -27,40 +27,40 @@ const initialData = {
     articles: [
       {
         id: 1,
-        avatar: require("../assets/instagram-feed-images/avatar1.jpg"),
-        name: '5.min.craft',
-        image: require("../assets/instagram-feed-images/img1.jpg"),
+        avatar: require("../assets/instagram-clone-images/avatar1.jpg"),
+        name: 'Younes Labiod',
+        image: require("../assets/instagram-clone-images/img1.jpg"),
         likes: 10,
         commentCount: 2,
         comments:'View all 0 comments'
       },
       {
         id: 2,
-        avatar: require("../assets/instagram-feed-images/avatar2.jpg"),
+        avatar: require("../assets/instagram-clone-images/avatar2.jpg"),
         name: 'tommy girl',
-        image: require("../assets/instagram-feed-images/img2.jpg"),
+        image: require("../assets/instagram-clone-images/img2.jpg"),
         likes: 3,
         commentCount: 200,
         comments:'View all 0 comments'
       },
       {
         id: 3,
-        avatar: require("../assets/instagram-feed-images/avatar3.jpg"),
+        avatar: require("../assets/instagram-clone-images/avatar3.jpg"),
         name: 'ninja mertyle',
-        image: require("../assets/instagram-feed-images/img3.jpg"),
+        image: require("../assets/instagram-clone-images/img3.jpg"),
         likes: 34,
         commentCount: 5,
         comments:'View all 0 comments'
       },
       {
         id: 4,
-        avatar: require("../assets/instagram-feed-images/avatar2.jpg"),
+        avatar: require("../assets/instagram-clone-images/avatar2.jpg"),
         name: 'britney pears',
-        image: require("../assets/instagram-feed-images/img4.jpg"),
+        image: require("../assets/instagram-clone-images/img4.jpg"),
         likes: 50,
         commentCount: 665,
         comments:'View all 0 comments'
-      },    
+      },
     ], 
 }
 
